57eb610aac113f07c1fb50d5375af04a81d4f84e
DrNH4CK3R <72782845+DrNH4CK3R@users.noreply.github.com>
2021-07-02 16:08:27 +0530

    Insta-Dp
    Insta-Dp

---
+++ instadp.py
@@ -0,0 +1,23 @@
+
+import sys
+import os
+import time
+import instaloader
+
+os.system("clear")
+
+os.system("figlet Insta-Dp ")
+print('                         ')
+print(' Author: DrNH4CK3R       ')
+print('                         ')
+print('---  ---  ---  ---  ---  ')
+
+
+
+dp = instaloader.Instaloader()
+
+acc = input('Enter the Account Username: ')
+
+print('                                   ')
+print('Searching for the given Username...')
+time.sleep(3)
